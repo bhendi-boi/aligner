226d598be08d8e65cb45cadffcb936f7a9a5133d
bhendi-boi <bhendiisgreen@gmail.com>
2025-03-27 11:21:03 +0530

    added `apb_rw_seq.sv`

---
+++ apb_rw_seq.sv
@@ -0,0 +1,28 @@
+class apb_rw_seq extends uvm_sequence #(transaction);
+    `uvm_object_utils(apb_rw_seq)
+
+    apb_transaction tr;
+
+    function new(string name = "apb_rw_seq");
+        super.new(name);
+        `uvm_info("Dummy sequence", "Constructed apb_rw_seq", UVM_HIGH)
+    endfunction
+
+    task body();
+        tr = apb_transaction::type_id::create("tr");
+
+        // TODO: Write constraints for addr
+        // write
+        start_item(tr);
+        if (!tr.randomize() with {dir == 1;})
+            `uvm_fatal("APB_RW_Seq", "Randomisation failed")
+        finish_item(tr);
+
+        // read
+        start_item(tr);
+        if (!tr.randomize() with {dir == 0;})
+            `uvm_fatal("APB_RW_Seq", "Randomisation failed")
+        finish_item(tr);
+    endtask
+
+endclass
