df1642d6909878849167627faf541caf8bfd9757
bhendi-boi <bhendiisgreen@gmail.com>
2025-03-27 11:28:36 +0530

    added `test_pkg.sv`

---
+++ test_pkg.sv
@@ -0,0 +1,11 @@
+`include "uvm_macros.svh"
+`include "apb_pkg.sv"
+
+package test_pkg;
+    import uvm_pkg::*;
+    import apb_pkg::*;
+
+    `include "env.sv"
+    `include "apb_reg_access_test.sv"
+
+endpackage
