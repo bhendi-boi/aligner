e0297a26fa688853304a06a065e2c223d2b82929
bhendi-boi <bhendiisgreen@gmail.com>
2025-03-31 15:02:51 +0530

    added `no_of_tr` field to sequence

--- apb_rw_seq.sv
+++ apb_rw_seq.sv
@@ -2,31 +2,43 @@ class apb_rw_seq extends uvm_sequence #(apb_transaction);
     `uvm_object_utils(apb_rw_seq)
 
     apb_transaction tr;
+    int no_of_tr;
 
     function new(string name = "apb_rw_seq");
         super.new(name);
+        set_no_of_tr(1);
         `uvm_info("Dummy sequence", "Constructed apb_rw_seq", UVM_HIGH)
+    endfunction
+
+    function void set_no_of_tr(int no_of_tr);
+        this.no_of_tr = no_of_tr;
+    endfunction
+
+    function int get_no_of_tr();
+        return this.no_of_tr;
     endfunction
 
     task body();
         tr = apb_transaction::type_id::create("tr");
 
-        start_item(tr);
-        if (!tr.randomize() with {
-                dir == 1;
-                addr inside {32'h0, 32'hC, 32'hF0, 32'hF4};
-            })
-            `uvm_fatal("APB_RW_Seq", "Randomisation failed")
-        finish_item(tr);
+        repeat (no_of_tr) begin
+            start_item(tr);
+            if (!tr.randomize() with {
+                    dir == 1;
+                    addr inside {32'h0, 32'hC, 32'hF0, 32'hF4};
+                })
+                `uvm_fatal("APB_RW_Seq", "Randomisation failed")
+            finish_item(tr);
 
-        // read
-        start_item(tr);
-        if (!tr.randomize() with {
-                dir == 0;
-                addr inside {32'h0, 32'hC, 32'hF0, 32'hF4};
-            })
-            `uvm_fatal("APB_RW_Seq", "Randomisation failed")
-        finish_item(tr);
+            // read
+            start_item(tr);
+            if (!tr.randomize() with {
+                    dir == 0;
+                    addr inside {32'h0, 32'hC, 32'hF0, 32'hF4};
+                })
+                `uvm_fatal("APB_RW_Seq", "Randomisation failed")
+            finish_item(tr);
+        end
     endtask
 
 endclass
